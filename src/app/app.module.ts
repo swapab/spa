ab6f530ad83203000373ca11636eea6f81f8bb10
Swapnil Abnave <swap.it@hotmail.com>
2017-09-09 01:33:47 +0530

    Angular CLI Deployment: Host Your Angular 2 App on Heroku

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { SubrouteComponent } from './subroute/subroute.component';
+
+import { ROUTES } from './app.routes';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SubrouteComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(ROUTES)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
